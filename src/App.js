6c15e78938df8fc67ffaa975ec129a769f341930
Kufreabasi <engineer_kufre@yahoo.com>
2020-12-11 21:43:03 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useMemo} from "react";
 import './App.css';
 import Count from "./Count";
 import Text from './Text';
@@ -6,10 +6,10 @@ import Text from './Text';
 function App() {
   const [data, setData] = useState([]);
   const [index, setIndex] = useState(0);
-  const [disablePrevButton,setDisablePrevButton] = useState("enable")
-  const [disableNextButton,setDisableNextButton] = useState("enable")
   const [color,setColor] = useState("#16a085")
-  const colors = ["#16a085", "#27ae60", "#2c3e50", "#f39c12", "#e74c3c", "#9b59b6", "#FB6964", "#342224", "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+  const colors = useMemo(() => {
+    return ["#16a085", "#27ae60", "#2c3e50", "#f39c12", "#e74c3c", "#9b59b6", "#FB6964", "#342224", "#472E32", "#BDBB99", "#77B1A9", "#73A857"]
+  },[]);
   useEffect(() => {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
     .then((res) => res.json())
@@ -17,29 +17,17 @@ function App() {
       setData(data.quotes);
     })
     .catch((err) => console.log(err));
+  },[])
 
-    if(index <= 0){
-      setDisablePrevButton("disable")
-    }
-    else {
-      setDisablePrevButton("enable")
-    }
-
-    if((index === data.length - 1)){
-      setDisableNextButton("disable")
-    }
-    else {
-      setDisableNextButton("enable")
-    }
-
-    setColor(colors[index % 12]);
-  }, [index])
+  useEffect(()=> {
+    setColor(colors[index % 12])
+  },[index,colors])
 
   const handleNext = () => {
     if(index === data.length - 1)
       return;
     else
-    setIndex(index + 1);
+      setIndex(index + 1);
   }
 
   const handlePrev = () => {
@@ -55,8 +43,8 @@ function App() {
         <Count current={index + 1} total={data.length} className="count" />
         <Text disp={data[index]} />
         <div className="Buttons">
-        <button onClick= {() => handlePrev()} className= {disablePrevButton} style={{backgroundColor:color}}>Previous</button>
-        <button onClick= {() => handleNext()} className={disableNextButton} style={{backgroundColor:color}}>Next</button>
+        <button onClick= {() => handlePrev()} className= {index <= 0 ? 'disable': ''} style={{backgroundColor:color}}>Previous</button>
+        <button onClick= {() => handleNext()} className={index === data.length - 1 ? 'disable':''} style={{backgroundColor:color}}>Next</button>
         </div>
       </div>
     </div>
